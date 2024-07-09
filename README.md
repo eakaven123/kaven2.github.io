61c9649b7e6321034a8c7d889b779674af174dfb
Emily Kaven <kaven2@illinois.edu>
2024-07-08 22:08:23 -0500

    added new page

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # kaven2.github.io
 cs416 project
+Below are sources that I used for my project as reference
+https://stackoverflow.com/questions/18680248/html-button-onclick-event used for clicking a button to go to next page
+https://www.w3schools.com/icons/ adding icons to html
--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 async function firstChart() {
-    //data is loaded
     console.log("hi");
     const data = await d3.csv("https://raw.githubusercontent.com/eakaven123/kaven2.github.io/main/2015.csv");
     console.log(data[1]);
+    console.log(data[2]);
+}
+
+//this specifies where you want the page to go to when the button is clicked from the first page to firstcharthtml page. page1->page2. this is called in index.html when button is clicked
+function secondPage() {
+    window.location.href = "firstchart.html";
 }
 
 firstChart();
